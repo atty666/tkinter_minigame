5772b0da0b49d1434a06b4f8a9dbe7d7f77d793c
denys <supriadkin.d@gmail.com>
2023-06-04 15:55:57 +0300

    added borber functions

--- main.py
+++ main.py
@@ -64,5 +64,34 @@ def within_y(co1, co2):
         return False
 
 
+def collided_left(co1, co2):
+    if within_y(co1, co2):
+        if co2.x2 >= co1.x1 >= co2.x1:
+            return True
+    return False
+
+
+def collided_right(co1, co2):
+    if within_y(co1, co2):
+        if co2.x1 <= co1.x2 <= co2.x2:
+            return True
+    return False
+
+
+def collided_top(co1, co2):
+    if within_x(co1, co2):
+        if co2.y2 >= co1.y1 >= co2.y1:
+            return True
+    return False
+
+
+def collided_bottom(y, co1, co2):
+    if within_x(co1, co2):
+        y_calc = co1.y2 + y
+        if co2.y1 <= y_calc <= co2.y2:
+            return True
+    return False
+
+
 game = Game()
 game.mainloop()
